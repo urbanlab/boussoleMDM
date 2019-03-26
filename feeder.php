e5c548f441e10bea682a360eefe8e20680dba94d
RACINE Pierre-Alexandre <pierrealexandreracine@gmail.com>
2019-03-26 10:31:52 +0100

    Import BDD.

---
+++ feeder.php
@@ -0,0 +1,10 @@
+<?php
+$donneesCompletes = array();
+
+$file = fopen('docs/Copie_de_BOUSSOLE.csv', 'r');
+while (($line = fgetcsv($file)) !== FALSE) {
+    array_push($donneesCompletes, $line);
+}
+
+//var_dump($donneesCompletes);
+echo json_encode($donneesCompletes);
